f5ead143edb916a68656d2bbd401f959741cfeb8
IrinaaMi <iro4ka.m.n@gmail.com>
2023-08-29 16:30:45 +0200

    Update cat-api.js

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,6 +1,8 @@
 const url = 'https://api.thecatapi.com/v1'
 const apiKey = 'uHSLi07StIOlriMPxJGxUbSYsHDs6AFx';
-
+const searchOptions = {
+    headers: { 'x-api-key': "live_8SXyJrMgCG3T9V2jXMl3jc5cQlrumRkCkAukd3ctVvCASmaZQlHgik550GXIhM3p" },
+}
 export function fetchBreeds() {
     return fetch(`${url}/breeds?api_key=${apiKey}`)
         .then(response => {
@@ -12,10 +14,10 @@ export function fetchBreeds() {
 };
 
 export function fetchCatByBreed(breedId) {
-    return fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`)
+    return fetch(`https://api.thecatapi.com/v1/images/search?limit=1&breed_ids=${breedId}`, searchOptions)
         .then(response => {
             if (!response.ok) {
-                throw new Error(response.status);
+                throw new Error('Network response was not ok');
             }
             return response.json();
         });
