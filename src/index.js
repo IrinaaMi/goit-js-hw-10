fc37590d066345a613358c9ac9d314bbbe9a34a0
IrinaaMi <iro4ka.m.n@gmail.com>
2023-08-29 16:47:59 +0200

    finished hv-10

--- src/index.js
+++ src/index.js
@@ -43,7 +43,7 @@ function onSelectBreed(event) {
             selector.classList.remove('is-hidden');
             const { url, breeds } = data[0];
 
-            divCatInfo.innerHTML = `<div class="box-img"><img src="${url}" alt="${breeds[0].name}" width="400"/></div>
+            divCatInfo.innerHTML = `<div class="box-img"><img src="${url}" alt="${breeds[0].name}" width="600"/></div>
       <div class="box">
       <h1>${breeds[0].name}</h1>
       <p>${breeds[0].description}</p>
